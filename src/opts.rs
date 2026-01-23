3009ae3a6023ba93d5f8c930f120a889b09e3469
jdrupal-dev <13871894+jdrupal-dev@users.noreply.github.com>
2026-01-23 10:45:19 +0100

    fix: Fix conflicting short options

--- src/opts.rs
+++ src/opts.rs
@@ -13,17 +13,17 @@ pub struct DrupalLspConfig {
     pub level: String,
 
     /// Uses stdio as the communication channel, will be as the default communication channel.
-    #[clap(short, long)]
+    #[clap(long)]
     pub stdio: bool,
 
     /// Use pipes (Windows) or socket files (Linux, Mac) as the communication channel.
     /// The pipe / socket file name is passed as the next arg or with --pipe=.
     /// Unsupported for now!
-    #[clap(short, long)]
+    #[clap(long)]
     pub pipe: Option<String>,
 
     /// Uses a socket as the communication channel. The port is passed as next arg or with --port=.
-    #[clap(short, long)]
+    #[clap(long)]
     pub socket: Option<u16>,
 
     /// The port to use for the socket connection.
